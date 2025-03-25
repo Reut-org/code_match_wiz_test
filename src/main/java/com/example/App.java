96829bd4f34dff693644360ead8d9d804681f206
reut-apiiro <reut.weibel@apiiro.com>
2025-03-25 15:09:20 +0200

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,1 +1,13 @@
+package com.example;
+
+import org.yaml.snakeyaml.Yaml;
+
+public class App {
+    public static void main(String[] args) {
+        Yaml yaml = new Yaml();
+        String input = "test: value";
+        Object output = yaml.load(input);
+        System.out.println(output);
+    }
+}
 
